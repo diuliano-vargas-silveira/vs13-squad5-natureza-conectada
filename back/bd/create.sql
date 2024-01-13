40a0cf895aa56e4cb3ee8e9a9371e74fcc85a445
Diuliano Vargas da Silveira <diulidsvargas@gmail.com>
2024-01-13 17:27:50 -0300

    feat: ajustando inserts

--- back/bd/create.sql
+++ back/bd/create.sql
@@ -163,34 +163,3 @@ CREATE SEQUENCE SEQ_ENTREGA_MUDA
     START WITH 1
     INCREMENT BY 1
     NOCACHE NOCYCLE;
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Alice Johnson', 'alice.johnson@example.com', 'senhadificil', 'ADMIN');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Bob Smith', 'bob.smith@example.com', 'senhadificil', 'CLIENTE');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Charlie Brown', 'charlie.brown@example.com', 'senhadificil', 'ESPECIALISTA');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'David Miller', 'david.miller@example.com', 'senhadificil', 'ADMIN');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Emily Davis', 'emily.davis@example.com', 'senhadificil', 'CLIENTE');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Frank Wilson', 'frank.wilson@example.com', 'senhadificil', 'ESPECIALISTA');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Grace Turner', 'grace.turner@example.com', 'senhadificil', 'ADMIN');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Henry Roberts', 'henry.roberts@example.com', 'senhadificil', 'CLIENTE');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Ivy Nelson', 'ivy.nelson@example.com', 'senhadificil', 'ESPECIALISTA');
-
-INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
-VALUES (SEQ_USUARIO.NEXTVAL, 'Jackie Turner', 'jackie.turner@example.com', 'senhadificil', 'ADMIN');
-
--- back/bd/dados.sql
+++ back/bd/dados.sql
@@ -1,31 +1,30 @@
--- Insert statements using the nextVal from the sequence
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Alice Johnson', 'alice.johnson@example.com', 'senhadificil', 'ADMIN');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Alice Johnson', 'alice.johnson@example.com', 'senhadificil', 'ADMIN');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Bob Smith', 'bob.smith@example.com', 'senhadificil', 'CLIENTE');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Bob Smith', 'bob.smith@example.com', 'senhadificil', 'CLIENTE');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Charlie Brown', 'charlie.brown@example.com', 'senhadificil', 'EESPECIALISTA');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Charlie Brown', 'charlie.brown@example.com', 'senhadificil', 'ESPECIALISTA');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'David Miller', 'david.miller@example.com', 'senhadificil', 'ADMIN');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'David Miller', 'david.miller@example.com', 'senhadificil', 'ADMIN');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Emily Davis', 'emily.davis@example.com', 'senhadificil', 'CLIENTE');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Emily Davis', 'emily.davis@example.com', 'senhadificil', 'CLIENTE');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Frank Wilson', 'frank.wilson@example.com', 'senhadificil', 'EESPECIALISTA');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Frank Wilson', 'frank.wilson@example.com', 'senhadificil', 'ESPECIALISTA');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Grace Turner', 'grace.turner@example.com', 'senhadificil', 'ADMIN');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Grace Turner', 'grace.turner@example.com', 'senhadificil', 'ADMIN');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Henry Roberts', 'henry.roberts@example.com', 'senhadificil', 'CLIENTE');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Henry Roberts', 'henry.roberts@example.com', 'senhadificil', 'CLIENTE');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Ivy Nelson', 'ivy.nelson@example.com', 'senhadificil', 'EESPECIALISTA');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Ivy Nelson', 'ivy.nelson@example.com', 'senhadificil', 'ESPECIALISTA');
 
-INSERT INTO VEM_SER.USUARIO (id_usuario, nome, email, senha, tipoUsuario)
-VALUES (VEM_SER.SEQ_USUARIO.NEXTVAL, 'Jackie Turner', 'jackie.turner@example.com', 'senhadificil', 'ADMIN');
+INSERT INTO USUARIO (id_usuario, nome, email, senha, tipo_usuario)
+VALUES (SEQ_USUARIO.NEXTVAL, 'Jackie Turner', 'jackie.turner@example.com', 'senhadificil', 'ADMIN');
 
