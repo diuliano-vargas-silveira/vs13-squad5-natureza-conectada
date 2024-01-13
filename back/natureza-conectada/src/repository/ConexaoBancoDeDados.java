c527134e2bcc8d7e6beb9c4932b863f819158152
Carlos-Academico <henrique.queiroz@academico.ifpb.edu.br>
2024-01-12 21:17:23 -0300

    fix: na classe conexaobancodedados, arquivo .jar adicionado.

--- back/natureza-conectada/src/repository/ConexaoBancoDeDados.java
+++ back/natureza-conectada/src/repository/ConexaoBancoDeDados.java
@@ -5,23 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBancoDeDados {
-    private static final String SERVER = "localhost";
-    private static final String PORT = "1521"; // Porta TCP padrão do Oracle
+    private static final String SERVER = "vemser-dbc.dbccompany.com.br";
+    private static final String PORT = "25000";
     private static final String DATABASE = "xe";
 
-    // Configuração dos parâmetros de autenticação
-    private static final String USER = "system";
+
+    private static final String USER = "VS_13_EQUIPE_5";
     private static final String PASS = "oracle";
-    private static final String SCHEMA = "VEM_SER";
+    private static final String SCHEMA = "EQUIPE_5";
 
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:oracle:thin:@" + SERVER + ":" + PORT + ":" + DATABASE;
-        // jdbc:oracle:thin:@localhost:1521:xe
 
-        // Abre-se a conexão com o Banco de Dados
+
         Connection con = DriverManager.getConnection(url, USER, PASS);
 
-        // sempre usar o schema vem_ser
+
         con.createStatement().execute("alter session set current_schema=" + SCHEMA);
 
         return con;
