9a661dc86511e248f3938d76a4cd0479c6bef7c9
Willian Cavalheiro <williancavalheiro1212@gmail.com>
2024-01-14 17:09:42 -0300

    fix: main limpa

--- back/natureza-conectada/src/Main.java
+++ back/natureza-conectada/src/Main.java
@@ -18,22 +18,8 @@ public class Main {
 
         // Menu.rodarAplicacao();
 
-        ServiceCliente serviceCliente = new ServiceCliente();
-
-
         try {
-//            Cliente cliente1 = serviceCliente.procurarPorID(3);
-//            Cliente cliente = new Cliente();
-//            cliente.setId(cliente1.getId());
 //
-//            cliente.setNome("Willian");
-//            cliente.setEmail("novoemail8@gmail.com");
-//            cliente.setSenha("senhadifemailicil");
-//            cliente.setCpf("12345678910");
-
-            //serviceCliente.adicionar(cliente);serviceCliente
-
-            serviceCliente.deletar(3);
 
         }catch (Exception e) {
             e.printStackTrace();
