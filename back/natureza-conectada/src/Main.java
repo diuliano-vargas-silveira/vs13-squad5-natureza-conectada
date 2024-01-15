36d3e94bd7b698fb228ddcb6ea55e3da3cbdd568
Luh Santos <luisasasilva19@gmail.com>
2024-01-15 07:26:06 -0300

    fix main

--- back/natureza-conectada/src/Main.java
+++ back/natureza-conectada/src/Main.java
@@ -1,20 +1,6 @@
-// import utils.Menu;
-
-import services.ServiceMudas;
-import utils.MockContainer;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        ServiceMudas serviceMudas = new ServiceMudas();
-
-
-        try {
-            MockContainer mockContainer = new MockContainer();
-            mockContainer.iniciaObjetos();
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+      
     }
 }
