b0773d8f6f69382e6cd7a5d7da123ef173ba2cdc
Pedro-Antonetti <pedroantonetti@gmail.com>
2024-01-23 18:40:11 -0300

    Ajustando application.properties

--- back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/controllers/EspecialistaController.java
+++ back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/controllers/EspecialistaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("especialista")
+@RequestMapping("/especialista")
 public class EspecialistaController {
 
     ServiceEspecialista serviceEspecialista;
