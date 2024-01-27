ae428108d572416194d03e463a84f082d5e330e5
lucarauj <lukas.martins193@gmail.com>
2024-01-26 22:21:54 -0300

    feat: Adicionando CRUD de entregas com springweb e swagger

--- back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/services/ServiceEntrega.java
+++ back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/services/ServiceEntrega.java
@@ -1,65 +1,103 @@
-
 package br.com.vemser.naturezaconectada.naturezaconectada.services;
 
+import br.com.vemser.naturezaconectada.naturezaconectada.dto.request.EntregaRequestDTO;
+import br.com.vemser.naturezaconectada.naturezaconectada.dto.response.AdminResponseDTO;
+import br.com.vemser.naturezaconectada.naturezaconectada.dto.response.EntregaResponseDTO;
+import br.com.vemser.naturezaconectada.naturezaconectada.enums.StatusEntrega;
 import br.com.vemser.naturezaconectada.naturezaconectada.exceptions.ErroNoBancoDeDados;
+import br.com.vemser.naturezaconectada.naturezaconectada.exceptions.InformacaoNaoEncontrada;
+import br.com.vemser.naturezaconectada.naturezaconectada.exceptions.RegraDeNegocioException;
+import br.com.vemser.naturezaconectada.naturezaconectada.models.Admin;
+import br.com.vemser.naturezaconectada.naturezaconectada.models.Cliente;
 import br.com.vemser.naturezaconectada.naturezaconectada.models.Entrega;
 import br.com.vemser.naturezaconectada.naturezaconectada.repository.EntregaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ServiceEntrega {
 
-    private EntregaRepository entregaRepository;
+    private final EntregaRepository entregaRepository;
+    private final ServiceCliente serviceCliente;
+    private final ObjectMapper objectMapper;
 
-    public ServiceEntrega(EntregaRepository entregaRepository) {
-        this.entregaRepository = entregaRepository;
-    }
+    public EntregaResponseDTO adicionar(EntregaRequestDTO entregaRequestDTO, int idEndereco) throws RegraDeNegocioException {
+        // Realizar verificação do endereço por id
+        // Realizar verificação do cliente por id
+        // Realizar verificação da muda por id
 
-    public void adicionar(Entrega entrega, Integer idEndereco) {
-        try{
-            this.entregaRepository.adicionar(entrega, idEndereco);
-            System.out.println("*** Entrega adicionada com sucesso ****");
-        }catch(Exception ex){
+        try {
+            entregaRequestDTO.setStatus(StatusEntrega.RECEBIDO);
+            Entrega entrega = objectMapper.convertValue(entregaRequestDTO, Entrega.class);
+            Entrega entregaProcessada = entregaRepository.adicionar(entrega, idEndereco);
 
-            ex.printStackTrace();
+            return objectMapper.convertValue(entregaProcessada, EntregaResponseDTO.class);
+        } catch (ErroNoBancoDeDados e) {
+            throw new RegraDeNegocioException("Erro ao adicionar entrega.");
         }
     }
 
+    public EntregaResponseDTO editar(int id, EntregaRequestDTO entregaRequestDTO) throws ErroNoBancoDeDados {
+        try {
+            Entrega entrega = procurar(id);
 
-    public void deletar(Integer id) {
+            Entrega entregaEntity = objectMapper.convertValue(entregaRequestDTO, Entrega.class);
+            Entrega entregaProcessada = entregaRepository.editar(id, entregaEntity);
+            entregaProcessada.setId(id);
+
+            return objectMapper.convertValue(entregaProcessada, EntregaResponseDTO.class);
+        } catch (Exception e) {
+            throw new ErroNoBancoDeDados("Erro ao editar entrega com Id: " + id);
+        }
+    }
+
+    public List<EntregaResponseDTO> listarTodos() throws ErroNoBancoDeDados {
+        try {
+            List<Entrega> entregas = entregaRepository.listar();
+
+            return entregas.stream()
+                    .map(entrega -> objectMapper.convertValue(entrega, EntregaResponseDTO.class))
+                    .collect(Collectors.toList());
+        } catch (ErroNoBancoDeDados e) {
+            throw new ErroNoBancoDeDados("Erro ao listar todas as entregas");
+        }
+    }
+
+    public EntregaResponseDTO procurarPorID(int id) throws ErroNoBancoDeDados {
+        Entrega entrega = procurar(id);
+
+        if (entrega == null) {
+            throw new InformacaoNaoEncontrada("Não existe nenhuma entregada com o ID: " + id);
+        }
+
+        EntregaResponseDTO entregaResponseDTO = objectMapper.convertValue(entrega, EntregaResponseDTO.class);
+        return entregaResponseDTO;
+    }
+
+    public void deletar(Integer id) throws ErroNoBancoDeDados {
+        Entrega entrega = procurar(id);
+
         try {
             this.entregaRepository.remover(id);
-        }catch(Exception ex){
-            ex.printStackTrace();
+        } catch (ErroNoBancoDeDados e) {
+            throw new ErroNoBancoDeDados("Não foi encontrado entrega para o id: " + id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public void editar(int id, Entrega entregaAtualizada) {
-        try{
-            this.entregaRepository.editar(id, entregaAtualizada);
-        }catch (Exception e){
-            e.printStackTrace();
-
-        }
-    }
-
-    public void listarTodos() {
-        List<Entrega> listaEntrega = new ArrayList<>();
+    private Entrega procurar(int id) throws ErroNoBancoDeDados {
         try {
-            listaEntrega = this.entregaRepository.listar();
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-        if(listaEntrega.size() == 0){
-            System.out.println("Não existe entregas registradas.");
-        }
-        else{
-            for(Entrega entregaAtual : listaEntrega){
-                System.out.println(entregaAtual.toString());
-            }
+            return entregaRepository.procurarPorId(id);
+        } catch (Exception ex) {
+            throw new ErroNoBancoDeDados("Nenhuma entrega encontrada para o Id: " + id);
         }
     }
 }
