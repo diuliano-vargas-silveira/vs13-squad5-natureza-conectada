624a47195deb8f67caf97880bf9f8e28979d6151
lucarauj <lukas.martins193@gmail.com>
2024-02-02 16:16:53 -0300

    Comentando Controllers

--- back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/controllers/EspecialistaController.java
+++ back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/controllers/EspecialistaController.java
@@ -1,45 +1,45 @@
-package br.com.vemser.naturezaconectada.naturezaconectada.controllers;
-
-import br.com.vemser.naturezaconectada.naturezaconectada.controllers.interfaces.IEspecialistaController;
-import br.com.vemser.naturezaconectada.naturezaconectada.dto.request.EspecialistaCreateDTO;
-import br.com.vemser.naturezaconectada.naturezaconectada.dto.response.EspecialistaDTO;
-import br.com.vemser.naturezaconectada.naturezaconectada.services.ServiceEspecialista;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
-
-@RestController
-@RequestMapping("/especialista")
-@RequiredArgsConstructor
-public class EspecialistaController implements IEspecialistaController {
-
-    private final ServiceEspecialista serviceEspecialista;
-
-    @GetMapping
-    public ResponseEntity<List<EspecialistaDTO>> listarTodos() throws Exception {
-        return new ResponseEntity<>(serviceEspecialista.listarTodos(), HttpStatus.OK);
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<EspecialistaCreateDTO> buscarPorId(@PathVariable  Integer id) throws Exception {
-        return new ResponseEntity<>(serviceEspecialista.procurar(id), HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<EspecialistaCreateDTO> novoEspecialista(@RequestBody @Valid EspecialistaCreateDTO dto) throws Exception {
-        return new ResponseEntity<>(this.serviceEspecialista.adicionar(dto),HttpStatus.OK);
-    }
-//    @DeleteMapping("/{id}") todo: fazer exclusão logica no Banco de dados
-//    public ResponseEntity<Void> deletarEspecialista(@PathVariable Integer id) throws Exception {
-//        this.serviceEspecialista.deletar(id);
-//        return ResponseEntity.ok().build();
+//package br.com.vemser.naturezaconectada.naturezaconectada.controllers;
+//
+//import br.com.vemser.naturezaconectada.naturezaconectada.controllers.interfaces.IEspecialistaController;
+//import br.com.vemser.naturezaconectada.naturezaconectada.dto.request.EspecialistaCreateDTO;
+//import br.com.vemser.naturezaconectada.naturezaconectada.dto.response.EspecialistaDTO;
+//import br.com.vemser.naturezaconectada.naturezaconectada.services.ServiceEspecialista;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/especialista")
+//@RequiredArgsConstructor
+//public class EspecialistaController implements IEspecialistaController {
+//
+//    private final ServiceEspecialista serviceEspecialista;
+//
+//    @GetMapping
+//    public ResponseEntity<List<EspecialistaDTO>> listarTodos() throws Exception {
+//        return new ResponseEntity<>(serviceEspecialista.listarTodos(), HttpStatus.OK);
 //    }
-
-    @PutMapping("/{idEspecialista}")
-    public ResponseEntity<EspecialistaCreateDTO> atualizarEspecialista (@PathVariable Integer idEspecialista,@RequestBody @Valid EspecialistaCreateDTO dto) throws Exception {
-        return new ResponseEntity<>(this.serviceEspecialista.editar(idEspecialista,dto),HttpStatus.OK);
-    }
-}
+//    @GetMapping("/{id}")
+//    public ResponseEntity<EspecialistaCreateDTO> buscarPorId(@PathVariable  Integer id) throws Exception {
+//        return new ResponseEntity<>(serviceEspecialista.procurar(id), HttpStatus.OK);
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<EspecialistaCreateDTO> novoEspecialista(@RequestBody @Valid EspecialistaCreateDTO dto) throws Exception {
+//        return new ResponseEntity<>(this.serviceEspecialista.adicionar(dto),HttpStatus.OK);
+//    }
+////    @DeleteMapping("/{id}") todo: fazer exclusão logica no Banco de dados
+////    public ResponseEntity<Void> deletarEspecialista(@PathVariable Integer id) throws Exception {
+////        this.serviceEspecialista.deletar(id);
+////        return ResponseEntity.ok().build();
+////    }
+//
+//    @PutMapping("/{idEspecialista}")
+//    public ResponseEntity<EspecialistaCreateDTO> atualizarEspecialista (@PathVariable Integer idEspecialista,@RequestBody @Valid EspecialistaCreateDTO dto) throws Exception {
+//        return new ResponseEntity<>(this.serviceEspecialista.editar(idEspecialista,dto),HttpStatus.OK);
+//    }
+//}
