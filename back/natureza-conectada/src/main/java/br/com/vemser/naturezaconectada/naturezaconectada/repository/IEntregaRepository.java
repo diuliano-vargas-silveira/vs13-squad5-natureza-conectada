39ed7eea4b3f0573f27d26ce8de33593c9761fba
lucarauj <lukas.martins193@gmail.com>
2024-02-03 22:02:11 -0300

    feat: Adicionando CRUD Entrega com springa data jpa

--- back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/repository/IEntregaRepository.java
+++ back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/repository/IEntregaRepository.java
@@ -1,21 +1,21 @@
-//package br.com.vemser.naturezaconectada.naturezaconectada.repository;
-//
-//import br.com.vemser.naturezaconectada.naturezaconectada.enums.StatusEntrega;
-//import br.com.vemser.naturezaconectada.naturezaconectada.models.Cliente;
-//import br.com.vemser.naturezaconectada.naturezaconectada.models.Entrega;
-//import br.com.vemser.naturezaconectada.naturezaconectada.models.Muda;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.sql.Connection;
-//import java.util.List;
-//
-//@Repository
-//public interface IEntregaRepository extends JpaRepository<Entrega, Integer> {
-//
-//    Entrega editarStatus(int idEntrega, StatusEntrega status);
-//
-//    void atualizarMudas( int idEntrega, List<Muda> mudas);
-//
-//    Cliente obterClienteDaEntrega(Connection conexao, int idEntrega);
-//}
+package br.com.vemser.naturezaconectada.naturezaconectada.repository;
+
+import br.com.vemser.naturezaconectada.naturezaconectada.enums.StatusEntrega;
+import br.com.vemser.naturezaconectada.naturezaconectada.models.Entrega;
+import br.com.vemser.naturezaconectada.naturezaconectada.models.Muda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IEntregaRepository extends JpaRepository<Entrega, Integer> {
+
+    @Query("UPDATE Entrega SET STATUS = :status WHERE ID_ENTREGA = :idEntrega")
+    Entrega editarStatus(@Param("status") StatusEntrega status, @Param("idEntrega") Integer idEntrega);
+
+//    void atualizarMudas(int idEntrega, List<Muda> mudas);
+
+}
