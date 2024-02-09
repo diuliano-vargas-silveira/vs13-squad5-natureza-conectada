c2b0fd4b9278e412129c300d0c95482fa77f5752
Pedro-Antonetti <pedroantonetti@gmail.com>
2024-02-08 21:00:09 -0300

    Adicionando Permissões

--- back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/security/SecurityConfiguration.java
+++ back/natureza-conectada/src/main/java/br/com/vemser/naturezaconectada/naturezaconectada/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.com.vemser.naturezaconectada.naturezaconectada.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,6 +30,30 @@ public class SecurityConfiguration {
                 .csrf().disable()
                 .authorizeHttpRequests((authz) -> authz
                         .antMatchers("/auth", "/").permitAll()
+                        .antMatchers("/admin/**").hasRole("ADMIN")
+                        .antMatchers("/auth").permitAll()
+                        .antMatchers(HttpMethod.POST,"/cliente").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.PUT,"/cliente/{id}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.PUT,"/cliente/status/{id}").hasAnyRole("CLIENTE", "ADMIN")
+                        .antMatchers(HttpMethod.GET,"/cliente").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET,"/cliente/{id}").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET,"/cliente/ativos").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.POST,"/contato/{idCliente}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.PUT,"/contato/{idContato}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.PUT,"/contato/remover/{idContato}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.GET,"/contato").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET,"/contato/{idCliente}").hasAnyRole("CLIENTE", "ADMIN")
+                        .antMatchers(HttpMethod.POST,"/endereco/{idCliente}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.PUT,"/endereco/{idEndereco}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.PUT,"/endereco/remover/{idEndereco}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.GET,"/endereco").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET,"/endereco/{idEndereco}").hasAnyRole("CLIENTE", "ADMIN")
+                        .antMatchers(HttpMethod.PUT,"/endereco/ativar/{id}").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET,"/endereco/ativos").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.POST,"/entrega/{idEndereco}").hasRole("CLIENTE")
+                        .antMatchers(HttpMethod.GET,"/entrega").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET,"/entrega/{idEntrega}").hasAnyRole("CLIENTE", "ADMIN")
+
                         .anyRequest().authenticated()
                 );
         http.addFilterBefore(new TokenAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
